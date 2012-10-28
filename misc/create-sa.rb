624d627418dea840cde094af7fefbca65741b009
Yu Xin <scaner@gmail.com>
2012-10-28 17:29:13 +0800

    pre split region in each 6 hours

--- misc/create-sa.rb
+++ misc/create-sa.rb
@@ -25,11 +25,34 @@ ca = HColumnDescriptor.new("a")
   desc.addFamily(c)
 end
 
+#----
+def hex(s)
+	return s.bytes.map {|c| '%02x' % c}.join
+end
+
+tsbegin = Time.gm(2012, 8, 10)
+tsend = Time.gm(2012, 10, 30)
+tsadd = 3600 * 6
+
+splits = Array.new
+
+while tsbegin < tsend do
+	splits << hex(tsbegin.strftime("%Y%m%d%H%M")).to_java_bytes
+	tsbegin += tsadd
+end
+
 
 if admin.tableExists(tablename)
   admin.disableTable(tablename)
   admin.deleteTable(tablename)
 end
-admin.createTable(desc)
+
+splitkeys = Java::byte[][splits.size].new
+for i in 0 .. splits.size - 1 do
+	splitkeys[i] = splits[i]
+end
+admin.createTable(desc, splitkeys)
 
 exit
+
+# vim: ts=2 sts=2 ai
