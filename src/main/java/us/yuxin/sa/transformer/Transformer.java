43f70adc4ee6929cb14245ba1b9880bfda298ce3
Xin Yu <scaner@gmail.com>
2012-10-31 10:48:38 +0800

    add setup api.

--- src/main/java/us/yuxin/sa/transformer/Transformer.java
+++ src/main/java/us/yuxin/sa/transformer/Transformer.java
@@ -1,10 +1,12 @@
 package us.yuxin.sa.transformer;
 
 import java.util.Map;
+import java.util.Properties;
 
 public interface Transformer {
   public Map<String, Object> transform(Map<String, Object> data, Map<String, Object> params);
 
+  public void setup(Properties conf);
   public void start();
   public void stop();
 }
